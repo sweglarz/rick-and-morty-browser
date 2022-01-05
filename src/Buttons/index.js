10718e2ac611077f00c0ce8b213f359389e07446
Szymon Węglarz <simon.weglarz@gmail.com>
2022-01-05 21:48:52 +0100

    styled components, and window function on scroll added

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,17 +1,23 @@
+import { StyledButtonWrapper, StyledButtons } from "./styled";
 
 export const Buttons = ({setPage}) => {
 
     const nextPage = () => {
         setPage(page => page +1);
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+          });
     }
 
     const previousPage = () => {
         setPage(page => page -1);
     }
     return (
-        <div>
-            <button onClick={previousPage}>Previous</button>
-            <button onClick={nextPage}>Next</button>
-        </div>
+        <StyledButtonWrapper>
+            <StyledButtons onClick={previousPage}>Previous</StyledButtons>
+            <StyledButtons onClick={nextPage}>Next</StyledButtons>
+        </StyledButtonWrapper>
     )
 } 
