b414834ee4e74bf3f2dc950fcc74a30d3a704bbe
Szymon Węglarz <simon.weglarz@gmail.com>
2022-01-06 16:18:42 +0100

    extra buttons from top replaced by search component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import { Buttons } from './Buttons';
 import { Characters } from './Characters';
 import { Container } from './Container';
 import { Header } from "./Header";
+import { Search } from './Search';
 import { useCharacters } from "./useCharacters";
 
 function App() {
-  const { data, setPage } = useCharacters();
+  const { data, setPage, setSearch } = useCharacters();
 
   return (
     <>
       <Header />
       <Container>
-        <Buttons setPage={setPage} />
+        <Search setSearch={setSearch}/>
         <Characters data={data} />
         <Buttons setPage={setPage} />
       </Container>
