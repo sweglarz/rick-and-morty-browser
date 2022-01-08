fde6365dcc24d236e70fc91de778f1230eda0a59
Szymon Węglarz <simon.weglarz@gmail.com>
2022-01-08 15:26:32 +0100

    initial commit

---
+++ src/useFilters.js
@@ -0,0 +1,20 @@
+export const status = ["Alive", 
+"Dead", 
+"Unknown"];
+
+export const gender = ["Female", 
+"Male", 
+"Genderless", 
+"Unknown"];
+
+export const species = [ "Human",
+    "Alien",
+    "Humanoid",
+    "Poopybutthole",
+    "Mythological",
+    "Unknown",
+    "Animal",
+    "Disease",
+    "Robot",
+    "Cronenberg",
+    "Planet",]
