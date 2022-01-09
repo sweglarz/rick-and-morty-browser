9ec686a28e4a32d36b913799ce41b95011fc7c46
Szymon Węglarz <simon.weglarz@gmail.com>
2022-01-09 15:39:50 +0100

    status shown in the picture

--- src/Characters/index.js
+++ src/Characters/index.js
@@ -2,18 +2,31 @@ import { Failure } from "../Failure";
 import {
     StyledWrapper,
     StyledCharacter,
-    StyledParagraph,
-    StyledImage
+    StyledParagraphName,
+    StyledImage,
+    StyledAliveStatus,
+    StyledDeadStatus,
+    StyledUnknownStatus
 } from "./styled";
 
 export const Characters = ({ data, search }) => {
+
     return (
         <StyledWrapper>
             {data ? (
                 data.map((character, index) => (
                     <StyledCharacter key={`key-${index}`}>
                         <StyledImage src={character.image} alt="" />
-                        <StyledParagraph>{character.name}</StyledParagraph>
+                        {character.status === "Alive" ? (
+                            <StyledAliveStatus>{character.status}</StyledAliveStatus>
+                        ) : ""}
+                        {character.status === "Dead" ? (
+                            <StyledDeadStatus>{character.status}</StyledDeadStatus>
+                        ) : ""}
+                        {character.status === "unknown" ? (
+                            <StyledUnknownStatus>{character.status}</StyledUnknownStatus>
+                        ) : ""}
+                        <StyledParagraphName >{character.name}</StyledParagraphName>
                     </StyledCharacter>
                 ))
             ) :
