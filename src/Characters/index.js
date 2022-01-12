676b9125b4345af3d23f6e67ed98a32c42eeb90e
Szymon Węglarz <simon.weglarz@gmail.com>
2022-01-12 21:30:01 +0100

    console log deleted

--- src/Characters/index.js
+++ src/Characters/index.js
@@ -13,42 +13,42 @@ import { FetchingError } from "../FetchingError";
 import { Loading } from "../Loading";
 
 export const Characters = ({ data, search }) => {
-    const {characters} = useCharacters();
-    console.log(characters.state)
+    const { characters } = useCharacters();
+
     return (
         <StyledWrapper>
-            {characters.state === "loading" 
-            ? (
-                <Loading/>
-            )
-            : (characters.state === "error" 
+            {characters.state === "loading"
                 ? (
-                    <FetchingError/>
+                    <Loading />
                 )
-                : (
-                    <>
-                        {data ? (
-                            data.map((character, index) => (
-                                <StyledCharacter key={`key-${index}`}>
-                                    <StyledImage src={character.image} alt="" />
-                                    {character.status === "Alive" ? (
-                                        <StyledAliveStatus>{character.status}</StyledAliveStatus>
-                                    ) : ""}
-                                    {character.status === "Dead" ? (
-                                        <StyledDeadStatus>{character.status}</StyledDeadStatus>
-                                    ) : ""}                                        {character.status === "unknown" ? (
-                                         <StyledUnknownStatus>{character.status}</StyledUnknownStatus>
-                                    ) : ""}
-                                    <StyledParagraphName >{character.name}</StyledParagraphName>
-                                </StyledCharacter>
-                            ))
-                            ) 
-                            : (
-                                <Failure search={search} />
+                : (characters.state === "error"
+                    ? (
+                        <FetchingError />
+                    )
+                    : (
+                        <>
+                            {data ? (
+                                data.map((character, index) => (
+                                    <StyledCharacter key={`key-${index}`}>
+                                        <StyledImage src={character.image} alt="" />
+                                        {character.status === "Alive" ? (
+                                            <StyledAliveStatus>{character.status}</StyledAliveStatus>
+                                        ) : ""}
+                                        {character.status === "Dead" ? (
+                                            <StyledDeadStatus>{character.status}</StyledDeadStatus>
+                                        ) : ""}                                        {character.status === "unknown" ? (
+                                            <StyledUnknownStatus>{character.status}</StyledUnknownStatus>
+                                        ) : ""}
+                                        <StyledParagraphName >{character.name}</StyledParagraphName>
+                                    </StyledCharacter>
+                                ))
                             )
-                        }
-                    </>
-                ))
+                                : (
+                                    <Failure search={search} />
+                                )
+                            }
+                        </>
+                    ))
             }
         </StyledWrapper>
     );
