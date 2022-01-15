988f5c05d520fad54cb2e88b10cb5c06132cc234
Szymon Węglarz <simon.weglarz@gmail.com>
2022-01-15 18:42:19 +0100

    some colors added

--- src/theme.js
+++ src/theme.js
@@ -5,11 +5,13 @@ export const theme = {
         DustyGray: "#969696",
         EasternBlue: "#22A2BD",
         Conifer: "#6FE147",
+        Limeade: "#31B602",
+        JapaneseLaurel: "#2C9906",
         White: "#ffffff",
         Black: "#000000",
         BlackLight: "#00000091",
         EasternBlueLight: "#22a2bd99",
-        EasterBlueLightPlus: "#22a2bdd6",
+        EasternBlueLightPlus: "#22a2bde8",
         EasternBlueDark: "#1f91aa"
     },
     breakpoints: {
